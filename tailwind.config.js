b612fae699938440493e479a1dfdbace79104029
Robin Millette <robin@millette.info>
2020-04-15 07:17:06 -0400

    Add breakpoint.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ module.exports = {
   theme: {
     screens: {
       sm: "560px", // IPhone 5 width
+      md: "768px", // 2nd breakpoint for background image
     },
     container: {
       center: true,
