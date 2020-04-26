732d6e138a8698d42219989403e16621c7e7842b
Robin Millette <robin@millette.info>
2020-04-26 14:31:54 -0400

    Updates

--- main.js
+++ main.js
@@ -2,5 +2,7 @@
 
 document.forms[0].onsubmit = (ev) => {
   ev.preventDefault()
-  console.log("Hello Joe Submit!")
+  // console.log("Hello Joe Submit!")
+  alert("The service is not ready yet. If you found this you're seeing a development instance.")
+
 }
